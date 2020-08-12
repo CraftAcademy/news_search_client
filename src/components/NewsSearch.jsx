6523f8a050cb616297db8069951a316549c768f1
Sima <sima.boreiri@gmail.com>
2020-08-12 12:05:47 +0200

    modifies NewsSearch component and define onchangehandler and show result

--- src/components/NewsSearch.jsx
+++ src/components/NewsSearch.jsx
@@ -1,19 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { Button, Input } from 'semantic-ui-react'
+import axios from 'axios'
 
-const NewsSearch = () => {
+ class NewsSearch extends Component {
+   state = {
+     searchTerm: "",
+     articles: []
+   }
+
+   onChangeHandler = (e) => {
+     this.setState({ searchTerm: e.target.value})
+   }
+
+  async performSearch() {
+     const result =  await axios.get("http://newsapi.org/v2/everything?q=${this.state.searchItem}&from=2020-07-12&sortBy=publishedAt&apiKey==eae1a29865074492af4d8f77081210c9");
+     this.setState({
+       articles: result.data.articles
+     })
+   }
+
+  render() {
+let  showSearchResult = this.state.articles.map(article => {
   return (
-    <>
+    <li>{article.title}</li>
+  )
+})
+    return (
+      <div>
+        
       <Input
         type="text"
         data-cy="news-search"
-        placeholder="Input search term" />
+        placeholder="Input search term"
+        name="search"
+        value={this.state.serachTerm}
+        onChange={this.onChangehandler} />
+       
       <Button
+      name="search"
+      onClick={this.showSearchResult}
         data-cy="search-submit">
         Search
         </Button>
-    </>
-  )
+       
+        <ul>
+          {showSearchResult}
+        </ul>
+    </div>
+    );
+  }
 }
-
-export default NewsSearch
+export default NewsSearch;
