4e802a55b600fe759e7d697d6658c1ac31c55ee1
viktor_sommar <vsommarbaillot@gmail.com>
2020-08-12 10:37:34 +0200

    Setting up yarn and cypress. Adding first tests and a bit of implementation code.

--- src/components/NewsSearch.jsx
+++ src/components/NewsSearch.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import { Button, Input } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Button, Input } from "semantic-ui-react";
+import axios from "axios";
 
-const NewsSearch = () => {
-  return (
-    <>
-      <Input
-        type="text"
-        data-cy="news-search"
-        placeholder="Input search term" />
-      <Button
-        data-cy="search-submit">
-        Search
-        </Button>
-    </>
-  )
+class NewsSearch extends Component {
+
+  searchNews = async () => {
+    const results = await axios.get(`http://newsapi.org/v2/everything?q=${this.state.search}&from=2020-07-12&sortBy=publishedAt&apiKey=8225ccbb856d480682e8488928542447`)
+    this.setState({ items: results.data.items })
+  }
+  
+  render() {
+    return (
+      <>
+        <Input
+          type="text"
+          data-cy="news-search"
+          placeholder="Input search term"
+        />
+        <Button data-cy="search-submit">Search</Button>
+      </>
+    );
+  }
 }
 
-export default NewsSearch
+export default NewsSearch;
