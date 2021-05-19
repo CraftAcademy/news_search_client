a49a20cf10373fef997812514f96e8d3aa6e974e
kim_haaga <kim.haaga@live.se>
2021-05-19 11:58:02 +0200

    Add search func and update text

--- cypress/integration/userCanSeeApplicationTitle.feature.js
+++ cypress/integration/userCanSeeApplicationTitle.feature.js
@@ -1,6 +1,6 @@
 describe('Application main view', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'http://newsapi.org/v2/everything?q=biden&**', {
+    cy.intercept('GET', 'https://newsapi.org/v2/everything?q=biden&from=2021-04-19&sortBy=publishedAt&**', {
       fixture: 'biden.json',
     });
     cy.visit('/');
@@ -15,7 +15,8 @@ describe('Application main view', () => {
     cy.get('[data-cy=search-submit]').click();
     cy.get('[data-cy=results-container]').within(() => {
       cy.get('[data-cy=article-0]').within(() => {
-        cy.get('[data-cy=article-title]').should('contain', 'US: Harris under scrutiny for tough-on-crime prosecutor past')
+        cy.get('[data-cy=article-title-0]').should('contain', 'Sanktionen: Biden-Regierung nach Bericht über Nord Stream 2 unter Druck')
+        cy.get('[data-cy=article-description-0]').should('contain', 'US-Präsident Biden hat Nord Stream 2 „einen schlechten Deal für Europa“ genannt. Nun sieht er sich Vorwürfen ausgesetzt, er habe die Ostsee-Pipeline nie wirklich verhindern wollen.')
       })
     })
   });
